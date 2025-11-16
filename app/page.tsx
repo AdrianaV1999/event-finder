9ad824a3314317afce8303e2f5aca635d1271401
Adriana <vadriana258@gmail.com>
2025-11-16 20:35:23 +0100

    homepage completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import ExploreBtn from "./components/ExploreBtn";
+import EventCard from "./components/EventCard";
+import events from "@/lib/constants";
 const Page = () => {
   return (
     <section>
@@ -11,6 +13,17 @@ const Page = () => {
       <p className="text-center mt-5">
         Hackathons, Meetups, Conferences — All Together!
       </p>
+      <ExploreBtn />
+      <div className="mt-30 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
