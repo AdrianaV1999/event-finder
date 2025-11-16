ad94b7598daeaea075ff8b19889b67c99b38a4c1
Adriana <vadriana258@gmail.com>
2025-11-16 19:37:27 +0100

    setup global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        Explore Your Next
+        <br />
+        Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, Conferences — All Together!
+      </p>
+    </section>
+  );
 };
 
 export default Page;
